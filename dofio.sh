e9140e9f77b0a398881c6e500314960ebbc0fd23
Justin Erenkrantz <justin@erenkrantz.com>
2016-08-25 10:42:17 -0400

    Default to a 64GB filesize to enable larger block and parallelization.

--- dofio.sh
+++ dofio.sh
@@ -7,6 +7,10 @@ if [ $# -ge 1 ]; then
   PARALLEL_COUNT=$1
 fi
 
+# The default is 8GB, but we want to test larger block sizes
+GIGABYTES=64
+let MEGABYTES=${GIGABYTES}*1024
+
 if [ ! -d par${PARALLEL_COUNT} ]; then
   mkdir par${PARALLEL_COUNT}
 fi
@@ -23,7 +27,7 @@ for i in `seq 1 $PARALLEL_COUNT`; do
   if [ ! -d disk$i ]; then
     echo "Missing disk$i ?"
   else
-    $DIRNAME/fio.sh -b fio -w $TESTDIR -o $OUTDIR & <<EOF
+    $DIRNAME/fio.sh -b fio -m ${MEGABYTES} -w $TESTDIR -o $OUTDIR & <<EOF
 y
 y
 EOF
